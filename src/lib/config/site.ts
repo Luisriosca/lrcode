72c9589bc5312b2a0eb63776b0d7c2d5fb356cfd
luis.rios@legux.com <luis.rios.9815@gmail.com>
2023-05-26 17:54:29 -0600

    Changing the site information on site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,15 +4,15 @@ export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'luisrioscode.xyz',
   title: 'luisrioscode',
-  subtitle: 'Welcome to my coding blog portfolio site',
-  lang: 'en-US',
+  subtitle: 'Ingeniero de Software y amante de las plantas',
+  lang: 'es-MX',
   description: 'Me especializo en solucionar problemas y coleccionar plantas 🌺',
   author: {
     avatar: '/assets/elpatron@640.jpeg',
     name: 'luisrioscode',
     status: '🦩',
     bio: `
-      🇲🇽 Software Engineer 🧑🏻‍💻
+      🇲🇽 Software Engineer≥ 🧑🏻‍💻
       <br>
       I really wish I was playing Tears of The Kindom right now!
     `
