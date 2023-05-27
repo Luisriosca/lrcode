dff5c92efd437cc9e5476f4f5848771f6bfc5266
luis.rios@legux.com <luis.rios.9815@gmail.com>
2023-05-26 18:52:10 -0600

    Adding a simple card template in portafolio page. Changes into routes/portolio/+page.

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -3,11 +3,22 @@
   import { fly } from 'svelte/transition'
 </script>
 
-<div class="flex flex-col flex-nowrap justify-center xl:flex-row xl:flex-wrap h-feed">
-  <div
-    in:fly={{ x: 25, duration: 300, delay: 500 }}
-    out:fly={{ x: 25, duration: 300 }}
-    class="flex-1 w-full max-w-screen-md order-first mx-auto xl:mr-0 xl:ml-8 xl:max-w-md">
-    <Profile />
-  </div>
+<div class="w-full p-4">
+  <main class="w-full flex flex-col h-screen content-center justify-center">
+    <div class="w-full sm:w-1/2 lg:w-1/3 rounded-xl m-auto">
+      <div class="bg-white rounded shadow px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="card m-auto bg-base-100 shadow-xl">
+          <figure><img src="/blog/me-blogging.webp" alt="plant" /></figure>
+          <div class="card-body">
+            <h1 class="card-title">Portafolio en construcción</h1>
+            <p>¿Te gustan las plantas?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-secondary">No 💀</button>
+              <button class="btn btn-primary">Sí 🌱</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </div>
